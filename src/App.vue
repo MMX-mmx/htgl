1b95186da175ad3e61ba6d079ac8f650fde34fcb
MMX-mmx <2668457546@qq.com>
2020-08-11 21:00:54 +0800

    后台管理

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <van-overlay :show="this.$store.state.loading" class="overlay">
+      <van-loading color="#F0F0F0" size="30" vertical>加载中</van-loading>
+    </van-overlay>
+
+    <keep-alive include="drag">
+      <router-view />
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+}
+.overlay {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
